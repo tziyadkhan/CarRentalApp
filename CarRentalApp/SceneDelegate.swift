5e3f9c98e1189c729452f603241d51c5ccddbb19
Ziyadkhan Taghiyev <ziyadxan.tagiyev@gmail.com>
2023-10-22 16:21:12 +0400

    Login part

--- CarRentalApp/SceneDelegate.swift
+++ CarRentalApp/SceneDelegate.swift
@@ -16,9 +16,29 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        if UserDefaults.standard.bool(forKey: "loggedIN") {
+            loginClicked(windowScene: windowScene)
+        } else {
+            loginPage(windowScene: windowScene)
+        }
     }
 
+    
+    func loginClicked(windowScene: UIWindowScene) {
+        let controller = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "TabBarController") as! TabBarController
+        self.window?.rootViewController = UINavigationController(rootViewController: controller)
+        self.window?.makeKeyAndVisible()
+    }
+    
+    func loginPage(windowScene: UIWindowScene) {
+        let controller = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "LoginPageController") as! LoginPageController
+        self.window?.rootViewController = UINavigationController(rootViewController: controller)
+        self.window?.makeKeyAndVisible()
+    }
+    
+    
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
         // This occurs shortly after the scene enters the background, or when its session is discarded.
